17f93ef7f9ac1ff79c542f3f9a53c6299f8b8387
purinliang <purinliang@gmail.com>
2021-04-22 02:42:36 +0800

    feat: 修改快速幂，新增快速乘、光速乘
    1. 使用的注意事项：见注释
    2. 修复了传入参数满足 x==m 时的小bug

--- 数学/01_快速幂.cpp
+++ 数学/01_快速幂.cpp
@@ -1,9 +1,30 @@
+/** 二进制快速幂 */
 ll qpow(ll x, ll n, ll m = MOD) {
 //    assert(m > 1 and n >= 0);
-//    if (x < 0 or x > m) x = (x % m + m) % m;
+//    if (x < 0 or x >= m) x = (x % m + m) % m;
     ll res = 1;
     for (; n; n >>= 1, x = x * x % m)
         if (n & 1) res = res * x % m;
     return res;
 }
-|
+
+/** 二进制快速乘 */
+ll qmul(ll a, ll b, ll m = MOD) {
+//    assert(m > 1);
+//    if (a < 0 or a >= m) a = (a % m + m) % m;
+//    if (b < 0 or b >= m) b = (b % m + m) % m;
+    ll res = 1;
+    for (; b; b >>= 1, a = (a + a) % m)
+        if (b & 1) res = (res + a) % m;
+    return res;
+}
+
+/** 光速乘 */
+ll qqmul(ll a, ll b, ll m = MOD) {
+//    assert(m > 1);
+//    if (a < 0 or a >= m) a = (a % m + m) % m;
+//    if (b < 0 or b >= m) b = (b % m + m) % m;
+    ull res = (ull) a * b - (unsigned)((long double)a / m * b + 0.5L) * m;
+    return res < m ? res : res + m;
+}
+
