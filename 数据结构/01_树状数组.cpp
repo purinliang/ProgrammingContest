b01556bcf55258369fac9ed4e51eca8ae68dfed4
purinliang <purinliang@gmail.com>
2021-04-25 15:15:50 +0800

    refactor: 修改 01_树状数组 ，改进其可读性

--- 数据结构/01_树状数组.cpp
+++ 数据结构/01_树状数组.cpp
@@ -3,45 +3,46 @@
 struct BinaryIndexTree {
     static const int MAXN = 5e5 + 5;
     int n;
-    ll sum[MAXN];
-    void Add(int x, int v) {
-        for (int i = x; i <= n; i += i & (-i)) sum[i] += v;
+    ll nd[MAXN];
+    void add(int x, int v) {
+        for (int i = x; i <= n; i += i & (-i)) nd[i] += v;
     }
-    ll Sum(int x) {
+    ll sum(int x) {
         ll res = 0;
-        for (int i = x; i; i -= i & (-i)) res += sum[i];
+        for (int i = x; i; i -= i & (-i)) res += nd[i];
         return res;
     }
-    void Init(int n) {
+    void init(int n) {
         this->n = n;
-        memset(sum, 0, sizeof(sum[0]) * (n + 1));
+        memset(nd, 0, sizeof(nd[0]) * (n + 1));
     }
 } bit;
 
+
 /** 计算逆序对 */
 // https://www.luogu.com.cn/problem/P1908
-struct BinaryIndexTree {
+struct ReversePairs {
     static const int MAXN = 5e5 + 5;
-    int m, b[MAXN], sum[MAXN];
-    void Add(int x, int v) {
-        for (int i = x; i <= m; i += i & (-i)) sum[i] += v;
+    int m, b[MAXN], nd[MAXN];
+    void add(int x, int v) {
+        for (int i = x; i <= m; i += i & (-i)) nd[i] += v;
     }
-    int Sum(int x) {
+    int sum(int x) {
         int res = 0;
-        for (int i = x; i; i -= i & (-i)) res += sum[i];
+        for (int i = x; i; i -= i & (-i)) res += nd[i];
         return res;
     }
-    ll Calc(int n, int *a) {
+    ll calc(int n, int *a) {
         for (int i = 1; i <= n; ++i) b[i] = a[i];
         sort(b + 1, b + 1 + n);
         m = unique(b + 1, b + 1 + n) - (b + 1);
-        memset(sum, 0, sizeof(sum[0]) * (m + 1));
+        memset(nd, 0, sizeof(nd[0]) * (m + 1));
         ll res = 0;
         for (int i = 1; i <= n; ++i) {
             int pos = lower_bound(b + 1, b + 1 + m, a[i]) - b;
-            res += i - 1 - Sum(pos), Add(pos, 1);
+            res += i - 1 - sum(pos), add(pos, 1);
         }
         return res;
     }
-} bit;
+} rp;
 
