e88ac91a32200fbb649121a7042ebc2b6051fbee
purinliang <purinliang@gmail.com>
2021-05-11 00:05:12 +0800

    refactor: 修改 01_戴克斯特拉算法 ，改进其可读性

--- 图论/01_戴克斯特拉算法.cpp
+++ 图论/01_戴克斯特拉算法.cpp
@@ -6,30 +6,41 @@ struct Graph {
     int n;
     vector<pil> G[GRAPH_MAXN];
     ll dis[GRAPH_MAXN];
+
     void init(int n) {
         this->n = n;
-        for (int i = 1; i <= n; ++i) G[i].clear();
+        for (int i = 1; i <= n; ++i) {
+            G[i].clear();
+        }
     }
-    void addEdge() {
-        int u, v;
-        ll w;
-        scanf("%d%d%lld", &u, &v, &w);
+    void addDirectedEdge(int u, int v, ll w) {
         G[u].emplace_back(v, w);
     }
-    void addEdge(int u, int v, ll w) {
+    void addUndirectedEdge(int u, int v, ll w) {
         G[u].emplace_back(v, w);
+        G[v].emplace_back(u, w);
     }
+
     void dijkstra(int s) {
         static priority_queue<pli, vector<pli>, greater<pli>> pq;
-        for (int i = 1; i <= n; ++i) dis[i] = LINF;
-        pq.emplace(dis[s] = 0, s);
+        for (int i = 1; i <= n; ++i) {
+            dis[i] = LINF;
+        }
+        dis[s] = 0;
+        pq.emplace(dis[s], s);
         while (!pq.empty()) {
             auto [disu, u] = pq.top();
             pq.pop();
-            if (disu > dis[u]) continue;
+            if (disu > dis[u]) {
+                continue;
+            }
             for (auto [v, w] : G[u]) {
-                if (dis[v] <= dis[u] + w) continue;
-                pq.emplace(dis[v] = dis[u] + w, v);
+                if (dis[v] <= dis[u] + w) {
+                    continue;
+                }
+                // WARNING: enqueue only if dis[v] > dis[u] + w
+                dis[v] = dis[u] + w;
+                pq.emplace(dis[v], v);
             }
         }
     }
